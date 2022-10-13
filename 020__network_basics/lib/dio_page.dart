65dc4da5bd53e9042fc2933e24f55f8e0d4d3395
Ingo Augsten <info@ingo-augsten.de>
2022-10-13 13:54:25 +0200

    FutureBuilder widget to play with plain Dio

--- 020__network_basics/lib/dio_page.dart
+++ 020__network_basics/lib/dio_page.dart
@@ -1,34 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:network_basics/dio_class.dart';
+import 'package:dio/dio.dart';
+import 'package:network_basics/SECRETS/secrets.dart';
 
 class DioPage extends StatelessWidget {
-  final dioClass = DioClass();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Dio'),),
-        body: Column(
-          children: [
-            SizedBox(height: 50),
-            Center(
-              child: Wrap(
-                spacing: 50,
-                alignment: WrapAlignment.center,
-                children: [
-
-                  ElevatedButton(
-                    onPressed: dioClass.getHttp,
-                    child: Text('getHttp'),
-                  ),
-
-
-                ],
-              ),
-            ),
-
-          ],
+        body: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              Text('Dio Page'),
+              const SizedBox(height: 50),
+              AsyncWidget(),
+            ],
+          ),
         )
     );
   }
 }
+
+
+class AsyncWidget extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return FutureBuilder<String>(
+        future: asyncFetch(),
+        builder: (context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            return Text(snapshot.data!);  // ignore that it can be null
+          } else {
+            return CircularProgressIndicator();
+          }
+        }
+    );
+  }
+}
+
+Future <String> asyncFetch() async {
+// var authOptions = BaseOptions(
+//   baseUrl: Secrets.urlPrefix,
+//   connectTimeout: 5000,
+//   receiveTimeout: 3000,
+// );
+//
+// try {
+// var response = await Dio().get('http://www.google.com');
+// print(response);
+// } catch (e) {
+// debugPrint(e.toString());
+// }
+
+  await Future.delayed(Duration(seconds: 2));
+  return 'async data';
+}
+
