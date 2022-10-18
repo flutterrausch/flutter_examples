78fcbbcf3ffb4f2ef48d82a5f7745de6e4db35fe
Ingo Augsten <info@ingo-augsten.de>
2022-10-18 19:22:06 +0200

    dio get monitoring vals (Bearer, query params)

--- 020__network_basics/lib/dio_page.dart
+++ 020__network_basics/lib/dio_page.dart
@@ -5,18 +5,18 @@ import 'package:jwt_decode/jwt_decode.dart';
 
 
 Future <String> dioConsoleStuff() async {
-  var response;  // Response<Map>? response;
   String retStr;
-  BaseOptions authOptions = BaseOptions(
-    baseUrl: Secrets.urlPrefix,
-  );
-  Dio dio = Dio(authOptions);
 
-  dio.interceptors.add(InterceptorsWrapper(
-    onError: (error, _) {print(error.message);},  // TODO response code handling
-    //onRequest: (request, _) {print("${request.method} ${request.path}");}, // both fail
-    //onResponse: (response, _) {print(response.data);}
-  ));
+  var response;  // Response<Map>? response;
+  BaseOptions options = BaseOptions(baseUrl: Secrets.urlPrefix,);
+  var interceptors = InterceptorsWrapper(
+      onError: (error, _) {print(error.message);},  // TODO response code handling
+  //onRequest: (request, _) {print("${request.method} ${request.path}");}, // both fail
+  //onResponse: (response, _) {print(response.data);}
+  );
+
+  Dio dio = Dio(options);
+  dio.interceptors.add(interceptors);
 
   // post auth request
   try {
@@ -24,23 +24,35 @@ Future <String> dioConsoleStuff() async {
       Secrets.authPath,
       data: {"username": Secrets.usr, "password": Secrets.pwd},  // Dio: body -> data
     );
-    print(response.toString());  // response.data
+    //print(response.toString());  // response.data Map
+    retStr = response.toString();
+  } catch (e) {  // NTH catch DioError
+    debugPrint('caught: ' + e.toString());
+    retStr = e.toString();
+  }
+
+  // get access token, to send Bearer later
+  Map bodyJsons = response.data;  // dio returns an already decoded map
+  final accessToken = bodyJsons['access'];  // jwt string
+  //debugPrint('accessToken: $accessToken');
+
+  // decode jwt - only needed for token refresh
+  debugPrint('accessJsons = ${Jwt.parseJwt(accessToken)}');
+  debugPrint('accessExpiryDate = ${Jwt.getExpiryDate(accessToken)}');
+  debugPrint('accessExpired = ${Jwt.isExpired(accessToken)}');
+
+  // get monitoring values using accessToken
+  try {
+    options.headers['Authorization'] = 'Bearer $accessToken';
+    response = await dio.get(
+      Secrets.monitoringValuesPath,
+      queryParameters: {'start': '2022-01-01', 'end': '2022-12-31'},
+    );
     retStr = response.toString();
   } catch (e) {
     debugPrint('caught: ' + e.toString());
     retStr = e.toString();
   }
-
-  // get access token
-  Map bodyJsons = response.data;  // no jsonDecode required, dio returns an already decoded map
-  final accessToken = bodyJsons['access'];  // jwt string
-  debugPrint('accessToken: $accessToken');
-
-  debugPrint('accessJsons = ${Jwt.parseJwt(accessToken)}');
-  debugPrint('accessExpiryDate = ${Jwt.getExpiryDate(accessToken)}');
-  debugPrint('accessExpired = ${Jwt.isExpired(accessToken)}');
-
-
 
   return retStr;
 }
